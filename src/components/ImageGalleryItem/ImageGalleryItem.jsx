84a1f60200d1a7bfb42fd2f0a5ab2dee46572e75
SergY29 <106399401+SergY29@users.noreply.github.com>
2023-01-03 19:04:55 +0200

    work 7

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -3,17 +3,19 @@ import { GalleryItem } from './ImageGalleryItem.styled';
 import { Component } from 'react';
 
 export class ImageGalleryItem extends Component {
-  toggleModal = () => {
-    this.props.onToggle(this.props.showModal);
-  };
+  // toggleModal = () => {
+  //   this.props.onToggle(this.props.showModal);
+  // };
 
   render() {
     const { image } = this.props;
     return (
-      <GalleryItem onClick={this.toggleModal}>
+      <GalleryItem>
         <img src={image} alt="" />
         {this.props.children}
       </GalleryItem>
     );
   }
 }
+
+// onClick={this.toggleModal}
